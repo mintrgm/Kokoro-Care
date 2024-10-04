24fda0472182a6b25c3d54ca026c27d7f6bb711a
Devendra Singh <devendra.616@gmail.com>
2024-10-04 07:34:56 +0530

    Add routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import { About, Doctors, Home,Login, Contact, MyProfile, MyAppointments, Appointment } from './pages/Index'
+
 
 const App = () => {
   return (
-    <>
-    </>
+    <div className='mx-4 sm:mx-[10%]'>
+     
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/doctors/:speciality' element={<Doctors />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/my-appointments' element={<MyAppointments />} />
+        <Route path='/appointment/:docId' element={<Appointment />} />
+        
+      </Routes>
+    </div>
   )
 }
 
