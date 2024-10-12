ba79d3ae2c5cdb0b89d38b7758afe17eab093684
Devendra Singh <devendra.616@gmail.com>
2024-10-12 20:24:11 +0530

    myappointment page with css

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -10,21 +10,21 @@ const MyAppointments = () => {
       <p className='pb-3 mt-12 font-medium text-zinc-700 border-b'>My appointments</p>
       <div>
         {doctors && doctors.slice(0,4).map((doc, index) => (
-          <div key={index} className='grid grid-cols-[1fr_2fr] gap-4 sm:gap-6 py-2 border-b'>
+          <div key={index} className='grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b'>
             <div>
-              <img src={doc.image} alt={doc.name} />
+              <img src={doc.image} alt={doc.name} className='w-32 bg-indigo-50'/>
             </div>
-            <div>
-              <p>{doc.name}</p>
+            <div className='flex-1 text-sm text-zinc-600'>
+              <p className='text-neutral-800 font-semibold'>{doc.name}</p>
               <p>{doc.speciality}</p>
-              <p>Address:</p>
-              <p>{doc.address.line1}</p>
-              <p>{doc.address.line2}</p>
-              <p><span>Date & Time: </span> 11 Nov 2024 | 11:30 AM</p>              
+              <p className='text-zinc-700 font-medium mt-1'>Address:</p>
+              <p className='text-xs'>{doc.address.line1}</p>
+              <p className='text-xs'>{doc.address.line2}</p>
+              <p className='text-sm mt-1'><span className='text-sm text-neutral-700 font-medium'>Date & Time: </span> 11 Nov 2024 | 11:30 AM</p>              
             </div>
-            <div>
-              <button>Pay Online</button>
-              <button>Cancel Appointment</button>
+            <div className='flex flex-col gap-2 justify-end'>
+              <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300 '>Pay Online</button>
+              <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-500 hover:text-white transition-all duration-300 '>Cancel Appointment</button>
             </div>
           </div>
         ))}
