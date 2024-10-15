97e717c89a1e63f43c16f63210d9e7dd95557451
Devendra Singh <devendra.616@gmail.com>
2024-10-15 06:43:39 +0530

    add JWT authentication for admin add doctors

---
+++ backend/middlewares/index.js
@@ -0,0 +1,4 @@
+import authAdmin from "./authAdmin.js";
+import upload from "./multer.js";
+
+export { authAdmin, upload };
--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,10 +1,10 @@
 import express from "express";
-import upload from "../middlewares/multer.js";
 import { addDoctor, loginAdmin } from "../controllers/adminController.js";
+import { authAdmin, upload } from "../middlewares/index.js";
 
 const adminRouter = express.Router();
 
-adminRouter.post("/add-doctor", upload.single("image"), addDoctor);
+adminRouter.post("/add-doctor", authAdmin, upload.single("image"), addDoctor);
 adminRouter.post("/login", loginAdmin);
 
 export default adminRouter;
