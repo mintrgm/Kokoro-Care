f6b9f5986eef37046ac9ae249a614b54a9c13db1
Devendra Singh <devendra.616@gmail.com>
2024-10-17 07:58:24 +0530

    add navbar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,14 +3,20 @@ import Login from "./pages/Login";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { AdminContext } from "./context/";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 const App = () => {
   const { aToken } = useContext(AdminContext);
 
   return aToken ? (
-    <>
+    <div className="bg-[#F8F9FD]">
+      <Navbar />
       <ToastContainer />
-    </>
+      <div className="flex items-start">
+        <Sidebar />
+      </div>
+    </div>
   ) : (
     <>
       <Login />
